f2e0b9ad5a07cac7f9ea768b5f434c8a6b8cf9f7
Rafael Thomas Goz Coutinho <rafael.coutinho@gmail.com>
2016-02-23 09:51:06 -0300

    North Web Admin

---
+++ src/main/webapp/js/na-app.js
@@ -0,0 +1,151 @@
+var jsonTransformQuery = function (data, headers) {
+    data = angular.fromJson(data);
+    var mainObj;
+    for (var key in data) {
+        if (data.hasOwnProperty(key)) {
+            mainObj = data[key];
+            break;
+        }
+    }
+    console.log("Table ", mainObj)
+    var resp = [];
+    var cols = mainObj.columns;
+    var records = mainObj.records;
+    for (var i = 0; i < records.length; i++) {
+        var recordsEntry = records[i];
+        var entry = {};
+        for (var j = 0; j < cols.length; j++) {
+            var col = cols[j];
+            var val = recordsEntry[j];
+            entry[col] = val;
+        }
+        resp.push(entry);
+
+    }
+
+
+    return resp;
+}
+
+var angularModule =
+    angular.module('adminApp', [, 'ngRoute', 'ui.bootstrap', 'ngResource']).constant("appConfigs", {
+        "context": "/northServer/api.php"
+    }).config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl: 'partials/main.html'
+
+        }).when('/etapas/', {
+            templateUrl: 'partials/etapas.html',
+            controller: 'EtapasCtrl'
+        }).when('/etapa/:id', {
+            templateUrl: 'partials/etapa.html',
+            controller: 'EtapaDetailsCtrl'
+        }).when('/localidades', {
+            templateUrl: 'partials/locations.html',
+            controller: 'LocationListCtrl'
+        }).when('/localidade/:id', {
+            templateUrl: 'partials/location.html',
+            controller: 'LocationDetailsCtrl'
+        }).otherwise({
+            redirectTo: '/'
+        });
+    }]).filter("sanitize", ['$sce', function ($sce) {
+        return function (htmlCode) {
+            return $sce.trustAsHtml(htmlCode);
+        }
+    }]).filter('textOrNumber', function ($filter) {
+        return function (input, fractionSize) {
+            if (isNaN(input)) {
+                return "-";
+            } else {
+                return $filter('number')(input, fractionSize);
+            }
+            ;
+        };
+    })
+        .service('EtapasService', ['$http', '$q', '$resource', 'appConfigs', function ($http, $q, $resource, appConfigs) {
+            return $resource('http://localhost/northServer/api.php/Etapa/:id', {}, {
+                query: {
+                    isArray: true,
+                    transformResponse: jsonTransformQuery
+                }
+            })
+        }])
+        .service('LocationService', ['$http', '$q', '$resource', 'appConfigs', function ($http, $q, $resource, appConfigs) {
+            return $resource('http://localhost/northServer/api.php/Local/:id', {}, {
+                query: {
+                    isArray: true,
+                    transformResponse: jsonTransformQuery
+                }
+            });
+
+        }])
+        .controller('EtapasCtrl', ['$scope', '$timeout', '$window', '$routeParams', 'EtapasService', '$location', function ($scope, $timeout, $window, $routeParams, EtapasService, $location) {
+            $('[data-toggle="tooltip"]').tooltip();
+            $scope.etapas = EtapasService.query();
+
+            $scope.nova = function () {
+                $location.path("/etapa/-1");
+            }
+        }])
+        .controller('EtapaDetailsCtrl', ['$scope', '$timeout', '$location', '$routeParams', 'EtapasService', 'LocationService', function ($scope, $timeout, $location, $routeParams, EtapasService, LocationService) {
+            $("[data-mask]").inputmask();
+            if ($routeParams.id == -1) {
+                $scope.etapa = {}
+            } else {
+                $scope.etapa = EtapasService.get({ id: $routeParams.id },
+                function(data){
+                    if(data.id_Local!=null && data.id_Local!=-1){
+                        $scope.location=LocationService.get({id:data.id_Local});
+                    }
+                });
+            }
+            $scope.saveData = function () {
+                EtapasService.save({ id: $routeParams.id != -1 ? $routeParams.id : null }, $scope.etapa);
+            }
+
+
+            $scope.associateLocal = function (location) {
+                $scope.etapa.id_Local = location.id;
+                $scope.location = location;
+                $('#pickLocal').modal('hide');
+            }
+
+            $scope.cancel = function () {
+                $location.path("/etapas");
+            }
+
+            $scope.removeLocal = function () {
+                $scope.etapa.id_Local = null;
+                $scope.location = null;
+                $('#pickLocal').modal('hide');
+            }
+            $('#pickLocal').modal({show:false});
+            $('#pickLocal').on('shown.bs.modal', function () {
+                    $scope.locations = LocationService.query();
+            })
+        }])
+        .controller('LocationListCtrl', ['$scope', '$timeout', '$location', '$routeParams', 'LocationService', function ($scope, $timeout, $location, $routeParams, LocationService) {
+            $scope.locations = LocationService.query();
+            $scope.novaLocalidade = function () {
+                $location.path("/localidade/-1");
+            }
+
+
+        }])
+        .controller('LocationDetailsCtrl', ['$scope', '$timeout', '$location', '$routeParams', 'LocationService', function ($scope, $timeout, $location, $routeParams, LocationService) {
+            if ($routeParams.id == -1) {
+                $scope.location = {}
+            } else {
+                $scope.location = LocationService.get({ id: $routeParams.id });
+            }
+            $scope.saveData = function () {
+                LocationService.save({ id: $routeParams.id != -1 ? $routeParams.id : null }, $scope.location);
+            }
+
+            $scope.cancel = function () {
+                $location.path("/localidades");
+            }
+
+        }])
+    ;
---
+++ src/main/webapp/js/run-admin.js
@@ -0,0 +1,81 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Defines the javascript files that need to be loaded and their dependencies.
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+require.config({
+    paths: {
+        angular: '../bower_components/angular/angular.min',
+        angularResources: '../bower_components/angular-resource/angular-resource.min',
+        angularRoute: '../bower_components/angular-route/angular-route.min',
+        angularMessages: '../bower_components/angular-messages/angular-messages.min',
+        lodash: '../bower_components/lodash/dist/lodash.min',
+        jQuery: '../bower_components/admin-lte/plugins/jQuery/jQuery-2.1.4.min',
+        inputMask: '../bower_components/admin-lte/plugins/input-mask/jquery.inputmask',
+        inputMaskDate: '../bower_components/admin-lte/plugins/input-mask/jquery.inputmask.date.extensions',
+        inputMaskPhone: '../bower_components/admin-lte/plugins/input-mask/jquery.inputmask.phone.extensions',
+        inputMaskExtensions: '../bower_components/admin-lte/plugins/input-mask/jquery.inputmask.extensions',
+        boostrap: '../bower_components/admin-lte/bootstrap/js/bootstrap.min',
+        uiboostrap: '../bower_components/angular-bootstrap/ui-bootstrap-tpls.min',
+        adminLte: '../bower_components/admin-lte/dist/js/app.min',
+        knob: '../bower_components/admin-lte/plugins/knob/jquery.knob',
+        'adminApp': "na-app"
+
+    },
+    shim: {
+        jQuery: {
+            exports: "jQuery"
+        },
+        angular: {
+            exports: "angular"
+        },
+        csrfInterceptor: {
+            deps: ['angular']
+        },
+        angularMessages: {
+            deps: ['angular']
+        },
+        angularResources: {
+            deps: ['angular']
+        },
+        boostrap: {
+            deps: ['jQuery']
+        },
+        adminLte: {
+            deps: ['boostrap']
+        },
+        inputMask: {
+            deps: ['jQuery']
+        },
+        inputMaskExtensions: {
+            deps: ['jQuery', 'inputMask']
+        },
+        inputMaskPhone: {
+            deps: ['jQuery', 'inputMask','inputMaskExtensions']
+        },
+        inputMaskDate: {
+            deps: ['jQuery', 'inputMask','inputMaskExtensions']
+        },
+
+        uiboostrap: {
+            deps: ['angular']
+        },
+
+        angularRoute: {
+            deps: ['angular']
+        },
+        knob: {
+            deps: ['jQuery']
+        },
+        adminApp: {
+            deps: ['lodash', 'angular', 'angularMessages', 'angularRoute', 'angularResources', 'uiboostrap', 'adminLte', 'knob', 'inputMaskDate', 'inputMaskPhone']
+        }
+    }
+});
+
+require(['adminApp'], function () {
+
+    angular.bootstrap(document.getElementById('adminApp'), ['adminApp']);
+
+});
